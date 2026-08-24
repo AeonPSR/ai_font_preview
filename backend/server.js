a8b1e4e9fd56c5816ee3ed06708f998b4f2e6561
Omargazzah02 <gazzahomar2001@gmail.com>
2026-08-24 16:52:24 +0100

    Add Cross origin

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "https://ai-font-preview-psi.vercel.app",
+  origin: "https://ai-font-preview.vercel.app",
   methods: ["GET", "POST", "OPTIONS"],
   allowedHeaders: ["Content-Type"]
 }));
