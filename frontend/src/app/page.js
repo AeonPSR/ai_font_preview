f654da861d2f3804f416869a12d0555c67f656d4
Omargazzah02 <gazzahomar2001@gmail.com>
2026-08-24 16:36:25 +0100

    Add Header component

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -103,9 +103,8 @@ export default function Home() {
               className="w-full h-24 p-2 border rounded-md dark:bg-gray-700 dark:text-white"
               disabled={isLoading}
             />
-            Error : 
-            <div className='text-red-500'> {error && error}</div>
           </div>
+
 
           <div className="bg-white dark:bg-gray-800 rounded-lg shadow-sm p-6">
             <label className="block text-sm font-medium mb-2 text-gray-700 dark:text-gray-300">
